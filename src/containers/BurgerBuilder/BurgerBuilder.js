7cac09464879eed3b8f9b6ff1b63f9f872eb6469
Jibran Usman <jibran.usman@hotmail.com>
2020-09-02 19:55:26 +0500

    Added checkout modal with backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -20,6 +22,7 @@ class BurgerBuilder extends Component {
 		},
 		totalPrice: 4,
 		purchaseable: false,
+		purchasing: false,
 	}
 
 	addIngredientHandler = (type) => {
@@ -46,6 +49,14 @@ class BurgerBuilder extends Component {
 		}
 	}
 
+	purchaseHandler = () => {
+		this.setState({ purchasing: true })
+	}
+
+	purchaseCancelledHandler = () => {
+		this.setState({ purchasing: false })
+	}
+
 	updatePurchaseState (ingredients) {
 		const sum = Object.keys(ingredients)
 			.map(igKey => {
@@ -68,13 +79,17 @@ class BurgerBuilder extends Component {
 
 		return(
 			<Fragment>
+				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelledHandler}>
+					<OrderSummary ingredients={this.state.ingredients} />
+				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					price={this.state.totalPrice}
 					purchaseable={this.state.purchaseable}
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
-					disabled={disabledInfo} />
+					disabled={disabledInfo}
+					ordered={this.purchaseHandler} />
 			</Fragment>
 		);
 	}
